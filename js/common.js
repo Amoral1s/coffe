b50fab0110646a58f489202c6327e2e8a4b5ab2f
Amoral1s <43203197+Amoral1s@users.noreply.github.com>
2019-04-09 12:55:30 +0300

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,251 @@
+$(document).ready(function(){
+  
+  $('.slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    prevArrow: "<img src='img/left.png' width='100px' height='100px' class='prev' alt='1'>",
+    nextArrow: "<img src='img/right.png' width='100px' height='100px' class='next' alt='2'>"
+  });
+  $(".open").click(function() {
+    if ($(".mobile").is(":visible")) {
+          $(".mobile").slideUp(600);
+          
+    } else {
+      $(".mobile").slideDown(600);
+    };
+    
+  });
+  $(".closex").click(function() {
+    if ($(".mobile").is(":visible")) {
+          $(".mobile").slideUp(600);
+          
+    } else {
+      $(".mobile").slideDown(600);
+    };
+    
+  });
+   $(".gallery-img-1").click(function() {
+    if ($(".gallery-popup-1").is(":visible")) {
+          $(".gallery-popup-1").slideUp(600);
+          
+    } else {
+      $(".gallery-popup-1").slideDown(600);
+    };
+    
+  });
+  $(".gallery-popup-1").click(function() {
+    if ($(".gallery-popup-1").is(":visible")) {
+          $(".gallery-popup-1").slideUp(600);
+          
+    } else {
+      $(".gallery-popup-1").slideDown(600);
+    };
+    
+  });
+   $(".gallery-img-2").click(function() {
+    if ($(".gallery-popup-2").is(":visible")) {
+          $(".gallery-popup-2").slideUp(600);
+          
+    } else {
+      $(".gallery-popup-2").slideDown(600);
+    };
+    
+  });
+  $(".gallery-popup-2").click(function() {
+    if ($(".gallery-popup-2").is(":visible")) {
+          $(".gallery-popup-2").slideUp(600);
+          
+    } else {
+      $(".gallery-popup-2").slideDown(600);
+    };
+    
+  });
+   $(".gallery-img-3").click(function() {
+    if ($(".gallery-popup-3").is(":visible")) {
+          $(".gallery-popup-3").slideUp(600);
+          
+    } else {
+      $(".gallery-popup-3").slideDown(600);
+    };
+    
+  });
+  $(".gallery-popup-3").click(function() {
+    if ($(".gallery-popup-3").is(":visible")) {
+          $(".gallery-popup-3").slideUp(600);
+          
+    } else {
+      $(".gallery-popup-3").slideDown(600);
+    };
+    
+  });
+   $(".gallery-img-4").click(function() {
+    if ($(".gallery-popup-4").is(":visible")) {
+          $(".gallery-popup-4").slideUp(600);
+          
+    } else {
+      $(".gallery-popup-4").slideDown(600);
+    };
+    
+  });
+  $(".gallery-popup-4").click(function() {
+    if ($(".gallery-popup-4").is(":visible")) {
+          $(".gallery-popup-4").slideUp(600);
+          
+    } else {
+      $(".gallery-popup-4").slideDown(600);
+    };
+    
+  });
+   $(".gallery-img-5").click(function() {
+    if ($(".gallery-popup-5").is(":visible")) {
+          $(".gallery-popup-5").slideUp(600);
+          
+    } else {
+      $(".gallery-popup-5").slideDown(600);
+    };
+    
+  });
+  $(".gallery-popup-5").click(function() {
+    if ($(".gallery-popup-5").is(":visible")) {
+          $(".gallery-popup-5").slideUp(600);
+          
+    } else {
+      $(".gallery-popup-5").slideDown(600);
+    };
+    
+  });
+   $(".gallery-img-6").click(function() {
+    if ($(".gallery-popup-6").is(":visible")) {
+          $(".gallery-popup-6").slideUp(600);
+          
+    } else {
+      $(".gallery-popup-6").slideDown(600);
+    };
+    
+  });
+  $(".gallery-popup-6").click(function() {
+    if ($(".gallery-popup-6").is(":visible")) {
+          $(".gallery-popup-6").slideUp(600);
+          
+    } else {
+      $(".gallery-popup-6").slideDown(600);
+    };
+    
+  });
+   $(".gallery-img-7").click(function() {
+    if ($(".gallery-popup-7").is(":visible")) {
+          $(".gallery-popup-7").slideUp(600);
+          
+    } else {
+      $(".gallery-popup-7").slideDown(600);
+    };
+    
+  });
+  $(".gallery-popup-7").click(function() {
+    if ($(".gallery-popup-7").is(":visible")) {
+          $(".gallery-popup-7").slideUp(600);
+          
+    } else {
+      $(".gallery-popup-7").slideDown(600);
+    };
+    
+  });
+   $(".gallery-img-8").click(function() {
+    if ($(".gallery-popup-8").is(":visible")) {
+          $(".gallery-popup-8").slideUp(600);
+          
+    } else {
+      $(".gallery-popup-8").slideDown(600);
+    };
+    
+  });
+  $(".gallery-popup-8").click(function() {
+    if ($(".gallery-popup-8").is(":visible")) {
+          $(".gallery-popup-8").slideUp(600);
+          
+    } else {
+      $(".gallery-popup-8").slideDown(600);
+    };
+    
+  });
+   $(".gallery-img-9").click(function() {
+    if ($(".gallery-popup-9").is(":visible")) {
+          $(".gallery-popup-9").slideUp(600);
+          
+    } else {
+      $(".gallery-popup-9").slideDown(600);
+    };
+    
+  });
+  $(".gallery-popup-9").click(function() {
+    if ($(".gallery-popup-9").is(":visible")) {
+          $(".gallery-popup-9").slideUp(600);
+          
+    } else {
+      $(".gallery-popup-9").slideDown(600);
+    };
+    
+  });
+   $(".gallery-img-10").click(function() {
+    if ($(".gallery-popup-10").is(":visible")) {
+          $(".gallery-popup-10").slideUp(600);
+          
+    } else {
+      $(".gallery-popup-10").slideDown(600);
+    };
+    
+  });
+  $(".gallery-popup-10").click(function() {
+    if ($(".gallery-popup-10").is(":visible")) {
+          $(".gallery-popup-10").slideUp(600);
+          
+    } else {
+      $(".gallery-popup-10").slideDown(600);
+    };
+    
+  });
+  
+   $(".gallery-img-11").click(function() {
+    if ($(".gallery-popup-11").is(":visible")) {
+          $(".gallery-popup-11").slideUp(600);
+          
+    } else {
+      $(".gallery-popup-11").slideDown(600);
+    };
+    
+  });
+  $(".gallery-popup-11").click(function() {
+    if ($(".gallery-popup-11").is(":visible")) {
+          $(".gallery-popup-11").slideUp(600);
+          
+    } else {
+      $(".gallery-popup-11").slideDown(600);
+    };
+    
+  });
+  
+   $(".gallery-img-12").click(function() {
+    if ($(".gallery-popup-12").is(":visible")) {
+          $(".gallery-popup-12").slideUp(600);
+          
+    } else {
+      $(".gallery-popup-12").slideDown(600);
+    };
+    
+  });
+  $(".gallery-popup-12").click(function() {
+    if ($(".gallery-popup-12").is(":visible")) {
+          $(".gallery-popup-12").slideUp(600);
+          
+    } else {
+      $(".gallery-popup-12").slideDown(600);
+    };
+    
+  });
+  
+     
+});
+
